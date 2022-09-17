f282cd6b1d7d6c763a7917baf8183eedba28cb02
Vasylchenko Serhii <darnki@gmail.com>
2022-09-17 08:58:07 +0300

    work on the theme

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { space, layout, color, typography } from 'styled-system'
+
+export const Box = styled.div`
+  ${color}
+  ${space}
+  ${layout}
+  ${typography}
+`;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,11 +2,22 @@ import ProfileStats from './ProfileStats/ProfileStats';
 import { string, shape } from 'prop-types';
 import css from './Profile.module.css';
 import { number } from 'prop-types';
+import { Box } from 'components/Box/Box';
 
 const Profile = ({ name, tag, location, avatar, stats }) => {
     return (
-        <section className={css.section}>
-            <div className={css.profile}>
+        <Box
+            mb={5}
+            as='section'
+        >
+            <Box
+                maxWidth={400}
+                mr='auto'
+                ml='auto'
+                textAlign='center'
+                fontSize='m'
+                color='text'
+            >
                 <div className="description">
                     <img
                     src={avatar}
@@ -21,8 +32,8 @@ const Profile = ({ name, tag, location, avatar, stats }) => {
             </div>
            
                 <ProfileStats stats={stats}/>
-            </div>
-        </section>
+            </Box>
+        </Box>
     );
 };
 
